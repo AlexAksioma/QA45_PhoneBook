89cff647a632d67c3cbf4e0ce78193935ba69f92
AlexAksioma <alexmedqwerty@gmail.com>
2024-11-25 20:36:28 +0200

    started registration

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,2 +1,15 @@
-package tests;public class RegistrationTests {
+package tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class RegistrationTests extends ApplicationManager {
+
+    @Test
+    public void registrationPositiveTest(){
+        new HomePage(getDriver()).clickBtnLoginHeader();
+        new LoginPage(getDriver()).typeLoginForm("my_email", "password");
+    }
 }
