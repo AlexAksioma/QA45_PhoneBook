b8241c8737318974cfdfcc312fe6d90d08372b28
AlexAksioma <alexmedqwerty@gmail.com>
2025-02-25 21:15:45 +0200

    delete scr shot4

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -20,7 +20,7 @@ public class RegistrationTests extends ApplicationManager {
         int i = new Random().nextInt(1000);
         new HomePage(getDriver()).clickBtnLoginHeader();
         new LoginPage(getDriver()).typeRegistrationForm("frodo_baggins_123"+i+"@gmail.com",
-                "Password123!111");
+                "Password123!");
         Assert.assertTrue(new ContactsPage(getDriver()).isSignOutPresent());
     }
 
